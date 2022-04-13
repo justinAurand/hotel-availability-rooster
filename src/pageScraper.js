3a996c2767ae74657931e8c1f43f9c8943847b44
Justin Aurand <justin.aurand@creditly.com>
2022-04-13 16:23:44 -0400

    ensure pages aren't cached

--- src/pageScraper.js
+++ src/pageScraper.js
@@ -1,6 +1,7 @@
 const scraperObject = {
 	async scraper(browser, url) {
 		let page = await browser.newPage();
+		await page.setCacheEnabled(false);
 
 		await page.goto(url);
 		let bodyHtml = await page.evaluate(() => document.documentElement.outerHTML);
