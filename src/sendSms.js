3c1cb6d3688a7334177bcc47482337a574cd4344
Justin Aurand <justin.aurand@gmail.com>
2022-09-17 00:02:52 -0400

    alphabetization

--- src/sendSms.js
+++ src/sendSms.js
@@ -5,8 +5,8 @@ async function sendSms(body) {
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
     // const myPhoneNumber = process.env.MY_PHONE_NUMBER;
-    const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
     const partnersPhoneNumber = process.env.PARTNERS_PHONE_NUMBER;
+    const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
     try {
         client.messages
